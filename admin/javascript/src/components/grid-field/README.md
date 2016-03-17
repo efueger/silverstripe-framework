2b330ba36f9f0d3c9fa548864e27ace28b0c5c98
David Craig <david@silverstripe.com>
2016-03-17 15:16:31 +1300

    Adds initial GridField React component.
    - Removes watchify because it wasn't working

--- admin/javascript/src/components/grid-field/README.md
+++ admin/javascript/src/components/grid-field/README.md
@@ -3,3 +3,11 @@
 This component is used to display structured data in an extendible table layout.
 
 ## Props
+
+### Headings (array)
+
+The column headings.
+
+### Rows (array)
+
+The table rows.
--- admin/javascript/src/sections/campaign-admin/controller.js
+++ admin/javascript/src/sections/campaign-admin/controller.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import SilverStripeComponent from 'silverstripe-component';
 import GridField from '../../components/grid-field';
+import GridFieldHeader from '../../components/grid-field-header';
+import GridFieldHeaderCell from '../../components/grid-field-header-cell';
+import GridFieldRow from '../../components/grid-field-row';
+import GridFieldCell from '../../components/grid-field-cell';
 
 class CampaignAdminContainer extends SilverStripeComponent {
 
+    constructor(props) {
+        super(props);
+
+        // TODO: This will be an AJAX call and it's response stored in state.
+        this.mockData = {
+            campaigns: [
+                {
+                    title: 'SilverStripe 4.0 release',
+                    description: 'All the stuff related to the 4.0 announcement',
+                    changes: 20
+                }
+            ]
+        };
+    }
+
     render() {
-        return (
-            <GridField />
-        );
+        const columnNames = ['title', 'changes'];
+
+        const headerCells = columnNames.map((columnName, i) => <GridFieldHeaderCell key={i}>{columnName}</GridFieldHeaderCell>);
+        const header = <GridFieldHeader>{headerCells}</GridFieldHeader>;
+
+        const rows = this.mockData.campaigns.map((campaign, i) => {
+            const cells = columnNames.map((columnName, i) => {
+                return <GridFieldCell key={i}>{campaign[columnName]}</GridFieldCell>
+            });
+            return <GridFieldRow key={i}>{cells}</GridFieldRow>;
+        });
+
+        return <GridField header={header} rows={rows} />;
     }
 
 }
-
-CampaignAdminContainer.propTypes = {};
 
 export default CampaignAdminContainer;

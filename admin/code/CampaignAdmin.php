7723dae0d147c98ea1b02b6b645816f0fa69d493
David Craig <david@silverstripe.com>
2016-03-17 17:22:44 +1300

    Adds REST endpoints

--- admin/code/CampaignAdmin.php
+++ admin/code/CampaignAdmin.php
@@ -1,5 +1,9 @@
 <?php
 
+use Convert;
+use SS_HTTPRequest;
+use SS_HTTPResponse;
+
 /**
  * Campaign section of the CMS
  *
@@ -8,13 +12,23 @@
  */
 class CampaignAdmin extends LeftAndMain implements PermissionProvider {
 
-	private static $allowed_actions = [];
+	private static $allowed_actions = [
+		'createCampaign',
+		'readCampaign',
+		'updateCampaign',
+		'deleteCampaign',
+	];
 
 	private static $menu_priority = 999;
 
 	private static $menu_title = 'Campaigns';
 
-	private static $url_rule = '/$Action/$ID/$OtherID';
+	private static $url_handlers = [
+		'POST item/$ID' => 'createCampaign',
+		'GET item/$ID' => 'readCampaign',
+		'PUT item/$ID' => 'updateCampaign',
+		'DELETE item/$ID' => 'deleteCampaign',
+	];
 
 	private static $url_segment = 'campaigns';
 
@@ -27,6 +41,66 @@ class CampaignAdmin extends LeftAndMain implements PermissionProvider {
 
 	public function getEditForm($id = null, $fields = null) {
 		return '';
+	}
+
+	/**
+	 * REST endpoint to create a campaign.
+	 *
+	 * @param SS_HTTPRequest $request
+	 *
+	 * @return SS_HTTPResponse
+	 */
+	public function createCampaign(SS_HTTPRequest $request) {
+		$response = new SS_HTTPResponse();
+		$response->addHeader('Content-Type', 'application/json');
+		$response->setBody(Convert::raw2json(['campaign' => 'create']));
+
+		return $response;
+	}
+
+	/**
+	 * REST endpoint to get a campaign.
+	 *
+	 * @param SS_HTTPRequest $request
+	 *
+	 * @return SS_HTTPResponse
+	 */
+	public function readCampaign(SS_HTTPRequest $request) {
+		$response = new SS_HTTPResponse();
+		$response->addHeader('Content-Type', 'application/json');
+		$response->setBody(Convert::raw2json(['campaign' => 'read']));
+
+		return $response;
+	}
+
+	/**
+	 * REST endpoint to update a campaign.
+	 *
+	 * @param SS_HTTPRequest $request
+	 *
+	 * @return SS_HTTPResponse
+	 */
+	public function updateCampaign(SS_HTTPRequest $request) {
+		$response = new SS_HTTPResponse();
+		$response->addHeader('Content-Type', 'application/json');
+		$response->setBody(Convert::raw2json(['campaign' => 'update']));
+
+		return $response;
+	}
+
+	/**
+	 * REST endpoint to delete a campaign.
+	 *
+	 * @param SS_HTTPRequest $request
+	 *
+	 * @return SS_HTTPResponse
+	 */
+	public function deleteCampaign(SS_HTTPRequest $request) {
+		$response = new SS_HTTPResponse();
+		$response->addHeader('Content-Type', 'application/json');
+		$response->setBody(Convert::raw2json(['campaign' => 'delete']));
+
+		return $response;
 	}
 
 }

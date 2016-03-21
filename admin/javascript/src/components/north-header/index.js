e9dc49f5e2a08cb3843f24047f9e77d2939116c9
scott1702 <scott@silverstripe.com>
2016-03-22 10:10:47 +1300

    Add breadcrumbs component with mock data

--- admin/javascript/src/components/north-header/index.js
+++ admin/javascript/src/components/north-header/index.js
@@ -1,21 +1,28 @@
 import React from 'react';
+import NorthHeaderBreadcrumbsComponent from '../north-header-breadcrumbs';
 import SilverStripeComponent from 'silverstripe-component';
 
 class NorthHeaderComponent extends SilverStripeComponent {
 
     render() {
-        //TODO: Create breadcrumbs component
         return (
             <div className="north-header-component">
-                <div className="cms-content-header-info">
-                    <div className="breadcrumbs-wrapper">
-                        <h2 id="page-title-heading">
-                            <span className="crumb">Campaigns</span>
-                        </h2>
-                    </div>
-                </div>
+                <NorthHeaderBreadcrumbsComponent crumbs={this.getBreadcrumbs()}/>
             </div>
         );
+    }
+
+    getBreadcrumbs() {
+        return [
+            {
+                text: 'Campaigns',
+                href: 'admin/campaigns'
+            },
+            {
+                text: 'March release',
+                href: 'admin/campaigns/show/1'
+            }
+        ]
     }
 
 }
